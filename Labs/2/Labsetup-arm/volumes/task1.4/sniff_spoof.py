2ddb7f3f5914b4881fc88a23383a7b62d7167358
Bikatr7 <Bikatr7@proton.me>
2025-03-06 11:35:39 -0700

    1.4, untested

---
+++ Labs/2/Labsetup-arm/volumes/task1.4/sniff_spoof.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+from scapy.all import *
+
+def spoof_icmp_reply(pkt):
+    if(pkt.haslayer(ICMP) and pkt[ICMP].type == 8):
+        print(f"[*] Detected ICMP Echo Request: {pkt[IP].src} -> {pkt[IP].dst}")
+        
+        ip = IP(src=pkt[IP].dst, dst=pkt[IP].src)
+        icmp = ICMP(type=0, code=0, id=pkt[ICMP].id, seq=pkt[ICMP].seq)
+
+        if(pkt.haslayer(Raw)):
+            data = pkt[Raw].load
+            reply = ip / icmp / data
+        else:
+            reply = ip / icmp
+        
+        print(f"[*] Spoofing ICMP Echo Reply: {pkt[IP].dst} -> {pkt[IP].src}")
+        send(reply, verbose=0)
+
+print("[*] Starting ICMP Sniff and Spoof program...")
+print("[*] Monitoring for ICMP Echo Requests...")
+sniff(filter="icmp", prn=spoof_icmp_reply, iface="eth0") 
